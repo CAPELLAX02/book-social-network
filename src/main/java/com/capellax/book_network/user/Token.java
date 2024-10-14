6d3d96bb15c05be3a6978611a3bb720cc3f407f5
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-14 11:27:03 +0300

    Implement the register method & Token entity builder annotation fix

--- src/main/java/com/capellax/book_network/user/Token.java
+++ src/main/java/com/capellax/book_network/user/Token.java
@@ -1,15 +1,13 @@
 package com.capellax.book_network.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
