a7f9df0395a9649edb5458fad2bd2804d0cd8cb2
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-15 12:44:33 +0300

    User registration and sending verification mail flow tested via Postman.

--- src/main/java/com/capellax/book_network/security/JwtFilter.java
+++ src/main/java/com/capellax/book_network/security/JwtFilter.java
@@ -31,6 +31,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
+            return;
         }
 
         final String authHeader = request.getHeader("Authorization");
@@ -39,6 +40,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
+            return;
         }
 
         jwt = authHeader.substring(7);
